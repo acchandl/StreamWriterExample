0eaf03d389006dcbb161a9cca54c3904542bd005
Alexis Chandler <alexiscchandler@gmail.com>
2016-12-01 15:27:12 -0500

    first commit

---
+++ StreamWriter Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamWriter_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            StreamWriter writer = new StreamWriter("number.txt");
+            using (writer)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    writer.WriteLine(i);
+                }
+            }
+
+          
+
+
+
+        }
+    }
+}
